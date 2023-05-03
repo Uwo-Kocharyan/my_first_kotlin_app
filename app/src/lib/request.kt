9db24cbf2f49c171c683ff3a514a2c5af3f0c00c
Uwo Kocharyan <uwokocharyan@gmail.com>
2023-05-03 17:23:04 +0400

    second commit

---
+++ app/src/lib/request.kt
@@ -0,0 +1,16 @@
+package src.lib
+//var pokeApi =
+//    "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+//late List pokedex;
+//void fetchPokemonData() {
+//    var url = Uri.https("raw.githubusercontent.com",
+//        "/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+//    http.get(url).then((value) {
+//        if (value.statusCode == 200) {
+//            var decodedJsonData = jsonDecode(value.body);
+//            pokedex = decodedJsonData['pokemon'];
+//            setState(() {});
+//        }
+//    });
+//}
+
--- app/src/main/java/com/example/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.mykotlinapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "User 1", "User 2", "User 3",
+            "User 4", "User 5"
+        )
+        val mListView = findViewById<ListView>(R.id.userList)
+        arrayAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, users
+        )
+        mListView.adapter = arrayAdapter
     }
 }
